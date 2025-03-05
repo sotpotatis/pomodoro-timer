0758edfe07873ef16bb7d86e7e6964a755c42ee2
sotpotatis <albin@albins.website>
2025-03-05 20:57:31 +0100

    Update Charlieplexing loop to work in more cases

--- code/sotpotatis-pomodoro-timer/sotpotatis-pomodoro-timer/main.c
+++ code/sotpotatis-pomodoro-timer/sotpotatis-pomodoro-timer/main.c
@@ -9,6 +9,8 @@
 #include "hardwareSetup.h"
 #include "hardwareConst.h"
 #include "ledHandlers.h"
+#define F_CPU 8000000UL
+#include <util/delay.h>
 /* 
 Function to initialize the hardware.
 Supposed to be called at bootup.
@@ -23,8 +25,17 @@ int main(void)
     // Perform hardware initialization
 
 	init();
-	setPinStates(0x4000, 1, 0);
-	setPinStates(0x4000, 1, 1);
-	setCharlieplexingState(1);
+	// Provide a heartbeat signal on one of the pins
+	// setPinStates(0x4000, 1, 0);
+	// setPinStates(0x4000, 1, 1);
+	// setPinStates(1<<PA0<<8, 1, 0);
+	// setPinStates(1<<PA1<<8, 1, 0);
+	// setPinStates(1<<PA1<<8, 1, 1);
+	// setPinStates(1<<PA0<<8, 0, 1);
+	while(1){
+	for (int i=0;i<11;i++){
+	setCharlieplexingState(i);
+	_delay_ms(500);
+	}}
 }
 
