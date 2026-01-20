666ef4320b62cab729fb8c9680fd53b7aae9f0dd
sotpotatis <albin@albins.website>
2026-01-20 19:28:03 +0100

    add: Backup PCB routing progress before board reversal...

--- code/sotpotatis-pomodoro-timer/sotpotatis-pomodoro-timer/hardwareConst.h
+++ code/sotpotatis-pomodoro-timer/sotpotatis-pomodoro-timer/hardwareConst.h
@@ -10,7 +10,7 @@ Defines hardware-related constants.
 		// ====== ADC Pin to use for button multiplexing ======
 		// Note that ADC pins simply use the format defined by Microchip
 		// (0-7 depending on if you want to use pin PA0-7)
-		#define BUTTON_ADC_PIN 0
+		#define BUTTON_ADC_PIN 5
 		// For Pure I/O pins (not ADC-pins), each "_PIN" definition follows the following format:
 		// The Attiny24A has 12 GPIO pins. Starting from PA we define a 16 bit number:
 		// 0bPA7|PA6....PA0|constant 0000 PB3|...PB0 (for example: 0b0000 0011 0000 0000 refers to PA0 and PA1)
@@ -27,7 +27,7 @@ Defines hardware-related constants.
 		#define LED_CHARLIEPLEXING_PIN_2 0x0002 // PB1
 		#define LED_CHARLIEPLEXING_PIN_3 0x8000 // PA7
 		#define LED_CHARLIEPLEXING_PIN_4 0x4000 // PA6
-		#define LED_CHARLIEPLEXING_PIN_5 0x2000// PA5
+		#define LED_CHARLIEPLEXING_PIN_5 0x0100// PA0
 		// Operates only on 8 bit registers
 		#define PA_PIN_MASK 0xFF
 		#define PB_PIN_MASK 0x0F
